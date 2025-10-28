9af2ca44f333f595e902ed283af58e3db2176302
Soton Ahmed <soton.m360ict@gmail.com>
2025-10-28 20:28:44 +0600

    create team

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tournament.views import TournamentViewSet
 from rules.views import RuleViewSet
+from team.views import TeamViewSet
 
 router = DefaultRouter()
 router.register(r'tournaments', TournamentViewSet)
 router.register(r'rules', RuleViewSet)
+router.register(r'teams', TeamViewSet)
 
 
 urlpatterns = [
--- bcc_project/settings.py
+++ bcc_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'api',
     'tournament',
     'rules',
+    'team',
 ]
 
 MIDDLEWARE = [
