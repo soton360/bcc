f4ee2f1db6385f28934e01acb58b964f20af9beb
Soton Ahmed <soton.m360ict@gmail.com>
2025-10-29 09:27:22 +0600

    player migrate

--- player/models.py
+++ player/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tournament.models import Tournament
 
 # Create your models here.
 class Player(models.Model):
@@ -6,6 +7,8 @@ class Player(models.Model):
     phone = models.CharField(max_length=50)
     image = models.ImageField(upload_to='player_images/', null=True, blank=True)
     bkash_transcation_id = models.CharField(max_length=50, null=True, blank=True)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='tournament_players', null=True, blank=True)
 
     def __str__(self):
         return self.name
+    
