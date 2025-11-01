3e28f13c63d6799b1fc36614b98fdb1dcc0b379f
Soton Ahmed <soton371@gmail.com>
2025-11-01 16:57:05 +0600

    remove contributor

--- contributor/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Contributor
-
-class ContributorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contributor
-        fields = '__all__'
