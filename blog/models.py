5f895d897f90d13f257607af79cd0d16b848a37f
Soton Ahmed <soton371@gmail.com>
2025-11-05 00:55:43 +0600

    add image

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='blog_images/')
     content = RichTextUploadingField(null=True, blank=True)
     published_date = models.DateTimeField(auto_now_add=True)
 
--- team/models.py
+++ team/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Team(models.Model):
     name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='team_logos/', null=True, blank=True)
 
     def __str__(self):
         return self.name
