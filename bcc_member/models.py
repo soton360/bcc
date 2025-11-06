ad3125c3db36686941183503599cca11cb75e106
Soton Ahmed <soton371@gmail.com>
2025-11-06 23:04:45 +0600

    remove role

--- bcc_member/models.py
+++ bcc_member/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from api.constants import CATEGORIES_CHOICES
 from role.models import Role
 
 # Create your models here.
 class BCCMember(models.Model):
     name = models.CharField(max_length=30)
     image = models.ImageField(upload_to='bcc_members/')
-    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True, related_name='bcc_member_roles')
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} - {self.role.name if self.role else 'No Role'}"
--- role/views.py
+++ role/views.py
@@ -2,9 +2,13 @@ from .models import Role
 from .serializers import RoleSerializer
 from api.permissions import IsAdminOrReadOnly
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category']
