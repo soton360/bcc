3634a0713bcf07b8ef6d273d681898e200354473
Soton Ahmed <soton371@gmail.com>
2025-11-21 18:34:26 +0600

    active filter

--- tournament/views.py
+++ tournament/views.py
@@ -2,12 +2,15 @@ from rest_framework import viewsets
 from .models import Tournament, TournamentRuleTitle, TournamentRule
 from .serializers import TournamentSerializer, TournamentRuleTitleSerializer, TournamentRuleSerializer
 from api.permissions import IsAdminOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class TournamentViewSet(viewsets.ModelViewSet):
     queryset = Tournament.objects.all().order_by('-created_at')
     serializer_class = TournamentSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['is_active']
 
 
 class TournamentRuleTitleViewSet(viewsets.ModelViewSet):
